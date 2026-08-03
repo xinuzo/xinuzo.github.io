a05d3267b55cce94c785a4b380166a0203251453
xinuzo <rendy.adinata88@gmail.com>
2026-08-03 16:09:42 +0700

    remove template folder

--- web.evanchen.cc/handouts/ProbabilisticMethod/genmatch.py
+++
@@ -1,4 +0,0 @@
-for i in range(5):
-    for j in range(5):
-        print("draw(A[%s]--B[%s], p);" % (i, j))
-    print()
--- web.evanchen.cc/scripts/sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rclone -v copy output/  web:
-rclone -v sync static/  web:static/
-rclone -v sync applets/ web:applets/
-rclone -v copyto static/icons/favicon.ico web:favicon.ico
-
-echo ""
-echo "Gentle reminder: EXTDIRS and handouts directory not sync'ed"
